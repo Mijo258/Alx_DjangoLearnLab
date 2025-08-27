dd6285460ad57e5a4714bbc2c68a017f2cf97d82
mohammed <mijogiddo@gmail.com>
2025-08-27 17:19:09 +0300

    utilizing views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
+from django.http import HttpResponse
+from django.views import listview, DetailView
+
+def list_books(request):
+    # the output should be a list of all books and their authors
+    books = Book.objects.all()
+    output = ', '.join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output)
+
+
+class librarylistview(listview):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+class librarydetailview(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
 
 # Create your views here.
