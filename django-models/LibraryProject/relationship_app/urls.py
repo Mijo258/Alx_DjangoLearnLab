59a577efa4d48684d16ae8a186f1397b14e8d9b2
mohammed <mijogiddo@gmail.com>
2025-08-28 18:04:43 +0300

    final website

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('dashboard/admin/', views.admin_view, name='admin_view'),
     path('dashboard/librarian/', views.librarian_view, name='librarian_view'),
     path('dashboard/member/', views.member_view, name='member_view'),
-    path('book/add/', views.book_add, name='book_add'),
-    path('book/edit/<int:pk>/', views.book_edit, name='book_edit'),
-    path('book/delete/<int:pk>/', views.book_delete, name='book_delete'),
+    path('add_book/', views.book_add, name='book_add'),
+    path('edit_book/<int:pk>/', views.book_edit, name='book_edit'),
+    path('delete_book/<int:pk>/', views.book_delete, name='book_delete'),
 ]
